fc743729a10c95dd1b73ed80acc40f80881f4474
Prince <uchepibe@gmail.com>
2025-06-13 20:20:16 +0100

    first commit

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import React from "react";
+
+export default function Home() {
+  return <div className="">
+    <Header />
+    <Footer />
+  </div>;
+}
